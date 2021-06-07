adf49685307a04cc00c2dddbeb377dfed7c2404a
sophiacantu <81716503+sophiacantu@users.noreply.github.com>
2021-06-07 17:33:32 -0400

    nav update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,16 @@
+import $ from "jquery";
 
+let $nav = $('nav{date-nav}');
+let isVisible = false;
+
+$(".nav-btns").on("click", function(){
+
+    if(isVisible === false){
+        $nav.sshoow();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+})
